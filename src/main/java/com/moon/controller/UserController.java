ea827ec3a544f5bd24c59dd05bd5fdc2cf54788e
Moon <dilbaay@gmail.com>
2021-07-06 22:30:35 +0200

    Consuming Messages from a Specific Partition

--- src/main/java/com/moon/controller/UserController.java
+++ src/main/java/com/moon/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.moon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.*;
@@ -47,5 +53,34 @@ public class UserController {
                         + message + "] due to : " + ex.getMessage());
             }
         });
+    }
+
+    @KafkaListener(topics = "hero_topic", groupId = "hero")   //We can implement multiple listeners for a topic, each with a different group Id
+   // @KafkaListener(topics = "topic1, topic2", groupId = "foo")  //one consumer can listen for messages from various topics:
+    public void listenGroupFoo(String message) {
+        System.out.println("Received Message in group hero: " + message);
+    }
+
+    @KafkaListener(topics = "coder_topic", groupId = "coder")  //Spring also supports retrieval of one or more message headers using the @Header annotation in the listener:
+    public void listenWithHeaders(
+            @Payload String message,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+        System.out.println(
+                "Received Message in group coder: " + message
+                        + " from partition: " + partition);
+    }
+
+   @KafkaListener(
+            topicPartitions = @TopicPartition(topic = "hero_topic",
+                    partitionOffsets = {
+                            @PartitionOffset(partition = "0", initialOffset = "0"),
+                            @PartitionOffset(partition = "3", initialOffset = "0")}),
+            containerFactory = "partitionsKafkaListenerContainerFactory")   //For a topic with multiple partitions, however, a @KafkaListener can explicitly subscribe to a particular partition of a topic with an initial offset:
+    public void listenToPartition(
+            @Payload String message,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+        System.out.println(
+                "Received Message for Listen To Partitions: " + message
+                        + " from partition: " + partition);
     }
 }
