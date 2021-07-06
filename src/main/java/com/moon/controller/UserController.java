0e4e12831a6b0d17c0b765283002d81fad897cfe
Moon <dilbaay@gmail.com>
2021-07-06 20:09:47 +0200

    ListenableFuture object added

--- src/main/java/com/moon/controller/UserController.java
+++ src/main/java/com/moon/controller/UserController.java
@@ -2,6 +2,9 @@ package com.moon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -26,7 +29,23 @@ public class UserController {
 
 
     @GetMapping("/hero")
-    public void sendMessage(String msg) {
-        kafkaTemplate.send(TOPIC2, msg);
+    public void sendMessage(@RequestParam("message") String message) {
+
+        ListenableFuture<SendResult<String, String>> future =
+                kafkaTemplate.send(TOPIC2, message);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("Unable to send message=["
+                        + message + "] due to : " + ex.getMessage());
+            }
+        });
     }
 }
